5dd169bb9c020b1b93acd4bd1db3a5241b5a6289
Anton Nesterov <arch.nesterov@gmail.com>
2019-08-29 03:19:50 +0300

    [macos:build] automatticaly resolve Qt and Julia versions

--- lib/build/mac.sh
+++ lib/build/mac.sh
@@ -1,17 +1,20 @@
 rm -rf lib/jlqml
 
-export QT_VERSION="5.13.0"
-export JL_PATH="/Applications/Julia-1.0.app/Contents/Resources/Julia/"
+export BREW=$(brew --prefix)
+export QT_VERSION=$(ls $BREW/Cellar/qt/ | tail -1)
+export JL_VERSION=$(ls /Applications/ | grep Julia | tail -1)
+export JL_PATH="/Applications/$JL_VERSION/Contents/Resources/Julia/"
+export PATH="$PATH:$JL_PATH"
 
-julia -e 'using Pkg; Pkg.add("CxxWrap")'
+julia -e "using Pkg; Pkg.activate(\"$ROOT_DIR\"); Pkg.add(\"CxxWrap\")"
 
 export JL_CXX_DIR="$(julia -e 'using CxxWrap; print(dirname(dirname(CxxWrap.libcxxwrap_julia)))')"
-export QT_DIR="$(brew --prefix)/Cellar/qt/$QT_VERSION"
+export QT_DIR="$BREW/Cellar/qt/$QT_VERSION"
 
 git clone https://github.com/barche/jlqml lib/jlqml
 cd lib/jlqml && mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="$JL_CXX_DIR;$QT_DIR;$JL_PATH" && make
 cd $ROOT_DIR
 export JLQML_DIR="$ROOT_DIR/lib/jlqml/build"
-julia -e "using Pkg; Pkg.activate(\"$ROOT_DIR\"); Pkg.build()"
 
+julia -e "using Pkg; Pkg.activate(\"$ROOT_DIR\"); Pkg.build()"
 echo "🏁 Everything is ready!"
